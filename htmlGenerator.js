97f2be7c9140bd5132c57e6c53fd12309df3621c
Aloha <utah.clayton@gmail.com>
2021-07-11 14:44:31 -0700

    styling completed. Team members will appear in own cards

--- htmlGenerator.js
+++ htmlGenerator.js
@@ -1,52 +1,52 @@
 const html = (data) => {
     const internArray = data.filter(employee => employee.getRole() === 'Intern')
     const engineerArray = data.filter(employee => employee.getRole() === 'Engineer')
-    console.log(internArray);
-    console.log(engineerArray);
+    const genEmployee = (employee) => {
+        let schoolCheck = "school" in employee;
+        const option = schoolCheck === true ? employee.school: employee.github
+
+        return `<div class="col-sm-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">${employee.getRole()}</h5>
+                <p class="card-text">${employee.name}</p>
+                <p class="card-text">${employee.id}</p>
+                <p class="card-text">${employee.email}</p>
+                <p class="card-text">${option}</p>
+            </div>
+        </div>
+    </div>`
+    }
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+        <title>Team Profile Generator</title>
     </head>
     <body>
-        <p> ${ data[0].name }
-        </p>
-        <p> ${ data[0].id }
-        </p>
-        <p> ${ data[0].email }
-        </p>
-        <p> ${ data[0].officeNumber }
-        </p>
-        <p> ${ data[0].getRole() }
-        </p>
-            ${ internArray.map(intern => {
-                return `<p> ${ intern.name }
-                </p>
-                <p> ${ intern.id }
-                </p>
-                <p> ${ intern.email }
-                </p>
-                <p> ${ intern.school }
-                </p>
-                <p> ${ intern.getRole() }
-                </p>`
-            }) }
-            ${ engineerArray.map(engineer => {
-               return  `<p> ${ engineer.name }
-                </p>
-                <p> ${ engineer.id }
-                </p>
-                <p> ${ engineer.email }
-                </p>
-                <p> ${ engineer.github }
-                </p>
-                <p> ${ engineer.getRole() }
-                </p>`
-            }) }
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${data[0].getRole()}</h5>
+                    <p class="card-text">${data[0].name}</p>
+                    <p class="card-text">${data[0].id}</p>
+                    <p class="card-text">${data[0].email}</p>
+                    <p class="card-text">${data[0].officeNumber}</p>
+                </div>
+            </div>
+        </div>
+        ${internArray.map(intern => {
+        return genEmployee(intern)
+    }).join("")}
+        ${engineerArray.map(engineer => {
+        return genEmployee(engineer)
+    }).join("")}
     </body>
     </html>`
 };
-module.exports = html 
+module.exports = html
+
